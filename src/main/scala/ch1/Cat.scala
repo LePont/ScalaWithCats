9d9a2d35c0212fccfff4efb22c4394622e137180
Eoin O'Connor <daedalusgroup@gmail.com>
2018-03-22 23:31:06 +0000

    Finish Ch1

--- src/main/scala/ch1/Cat.scala
+++ src/main/scala/ch1/Cat.scala
@@ -1,9 +1,20 @@
 package ch1
 
+import cats._
+import cats.implicits._
+
 final case class Cat(name: String, age: Int, colour: String)
 
 object Cat {
   implicit val catPrintable = new Printable[Cat] {
     def format(cat: Cat) = s"${cat.name} is a ${cat.age} year-old ${cat.colour} cat."
   }
+
+  implicit val catShow: Show[Cat] = Show.show(cat => s"${cat.name} is a ${cat.age} year-old ${cat.colour} cat.")
+
+  implicit val catEq: Eq[Cat] =
+    Eq.instance[Cat] {
+      (cat1, cat2) =>
+        (cat1.age === cat2.age) && (cat1.colour === cat2.colour) && (cat1.name === cat2.name)
+    }
 }
--- src/main/scala/ch1/GO.scala
+++ src/main/scala/ch1/GO.scala
@@ -2,10 +2,28 @@ package ch1
 
 import PrintableSyntax._
 import PrintableInstances._
+import cats.implicits._
 
 object GO extends App {
 
   val cat = Cat("Buster", 2, "Blue")
   cat.print
   "fvbb".print
+
+  val cat1 = Cat("Bluebell", 3, "Ginger")
+  println(cat1.show)
+
+  val cat11 = Cat("Garfield", 38, "orange and black")
+  val cat2 = Cat("Heathcliff", 33, "orange and black")
+
+  println(cat11 === cat2)
+  println(cat11 =!= cat2)
+
+  val optionCat1 = Option(cat1)
+  val optionCat2 = Option.empty[Cat]
+
+  println(optionCat1 === optionCat2)
+  println(optionCat1 =!= optionCat2)
+
+
 }
