4dfb1ad5fda2374658abc1258efc55104b2ad273
eoconnor <eoin.oconnor@zalando.ie>
2018-07-24 08:48:55 +0100

    State Monad work pt2

--- src/main/scala/ch4/GO.scala
+++ src/main/scala/ch4/GO.scala
@@ -6,6 +6,8 @@ import cats.{Eval, Id, Monad}
 object GO extends App {
 
   // Identity playground
+  println("**** IDENTITY ****")
+
   val f = List(1, 2, 3): Id[List[Int]]
   f.map(f => f + 1)
 
@@ -25,8 +27,8 @@ object GO extends App {
   // map and flatMap are the same mindblown :)
   // Id[A] is just A
 
-
   //EVAL
+  println("**** EVAL ****")
 
   def foldRightEval[A, B](as: List[A], acc: Eval[B])(fn: (A, Eval[B]) => Eval[B]): Eval[B] =
     as match {
@@ -48,13 +50,15 @@ object GO extends App {
   import cats.syntax.applicative._
   import cats.syntax.writer._
   import cats.instances.vector._
+  println("**** WRITERS ****")
 
   val Vector((log1, ans1), (log2 ,an2)) = Await.result(Future.sequence(Vector(
     Future(factorial(3).run),
     Future(factorial(7).run)
   )), 5.seconds)
 
-  println()
+  println(log1)
+  println(ans1)
 
   def slowly[A](body: => A) =
     try body finally Thread.sleep(100)
@@ -76,6 +80,7 @@ object GO extends App {
 
   import ch4.Readers._
 
+  println("**** READERS ****")
   val users = Map(
     1 -> "d",
     2 -> "c",
@@ -92,6 +97,21 @@ object GO extends App {
   println(checkLogin(1, "go").run(db))
   println(checkLogin(2, "fail").run(db))
 
+  // STATE
+  import States._
+  println("**** STATES ****")
 
+  println(evalOne("4").runA(Nil).value)
+  val program = for {
+    _ <- evalOne("42")
+    _ <- evalOne("4")
+    ans <- evalOne("+")
+  } yield ans
 
+  println(program.runA(Nil).value)
+
+  val programAll = evalAll(List("1", "2", "+", "3", "*"))
+  println(programAll.runA(Nil).value)
+
+  println(evalInput("2 4 * 12 6 + 1 2 + 20 1 - 3 6 + *"))
 }
--- src/main/scala/ch4/States.scala
+++ src/main/scala/ch4/States.scala
@@ -1,18 +1,31 @@
 package ch4
 
 import cats.data.State
-
+import cats.syntax.applicative._
 object States {
 
   type CalcState[A] = State[List[Int], A]
 
   def evalOne(sym: String): CalcState[Int] = {
     sym match {
-      case number => dealWithNumber(number.toInt)
       case "+" => dealWithOperator(_ + _)
+      case "-" => dealWithOperator(_ - _)
+      case "*" => dealWithOperator(_ * _)
+      case "/" => dealWithOperator(_ / _)
+      case number => dealWithNumber(number.toInt)
     }
-
   }
+
+  def evalAll(input: List[String]): CalcState[Int] = {
+    input.foldLeft(0.pure[CalcState]){(a, b) =>
+      a.flatMap(_ => evalOne(b))
+    }
+  }
+
+  def evalInput(input: String): Int = {
+    evalAll(input.split(" ").toList).runA(Nil).value
+  }
+
   private def dealWithNumber(number: Int): CalcState[Int] = {
     State[List[Int], Int] {
       stack => (number :: stack, number)
